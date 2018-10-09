e385e9312f8e3ad5e953287a5672ae14829f2238
Luis R. Gonzales <lr.gonzales55@gmail.com>
2018-10-09 09:32:18 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ rm vehicles.zip
 wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/non-vehicles.zip
 unzip non-vehicles.zip
 rm non-vehicles.zip
-gdrive download 1vDeLeBa_VjXlcY0n9ov3AJaCCiTYcnKQ
+gdrive download 1xa92jvqcFcU9cPa4pp74WejGjqe3BH1u
 cd ..
